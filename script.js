8b37bca1c27df391121003d33cabef0a4a9ce8bb
Raihan127 <aburaihan07cse@gmail.com>
2024-12-02 00:06:31 +0600

    Save to github

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToBirthday() {
+    document.getElementById('firstPage').style.display = 'none';
+    document.getElementById('birthdayPage').style.display = 'block';
+}
+
+function showSurprise() {
+    document.getElementById('surprise').style.display = 'block';
+    document.getElementById('surpriseButton').style.display = 'none';
+}
